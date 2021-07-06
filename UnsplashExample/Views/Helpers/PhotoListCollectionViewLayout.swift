217ef60473a6e2019eaf91352906258b0181b5f7
Derrick Wilde <derrick.wilde@gmail.com>
2021-07-06 16:38:59 -0500

    small layout change

--- UnsplashExample/Views/Helpers/PhotoListCollectionViewLayout.swift
+++ UnsplashExample/Views/Helpers/PhotoListCollectionViewLayout.swift
@@ -12,7 +12,7 @@ class PhotoListCollectionViewLayout: UICollectionViewFlowLayout {
     // I'm just making sure the content looks good on a varitey of devices and orientations here. Another approach would be to use adaptive layouts in storyboards, but that is a bit more time intensive. Also item size is easiest to edit here.
     override func prepare() {
         super.prepare()
-        self.itemSize = CGSize(width: 175, height: 175)
+        self.itemSize = CGSize(width: 172, height: 172)
         self.minimumInteritemSpacing = 1
         self.minimumLineSpacing = 1
         self.sectionInset.left = 8
